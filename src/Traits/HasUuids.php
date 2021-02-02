ab46d5916ae9341fe84c9036a61d54df4a25467d
Ian.H <ian.h@digiserv.net>
2021-02-02 15:41:24 +0000

    Initial commit

---
+++ src/Traits/HasUuids.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Traits;
+
+use Ramsey\Uuid\Uuid;
+
+trait HasUuids
+{
+    public static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(static function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = Uuid::uuid()->toString();
+            }
+        });
+    }
+
+    public function getIncrementing(): bool
+    {
+        return false;
+    }
+
+    public function getKeyType(): string
+    {
+        return 'uuid';
+    }
+}
